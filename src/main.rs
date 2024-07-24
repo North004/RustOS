8496a072484c650c496373873762b83fdff173eb
Jamie Grant <jamiegrant050@gmail.com>
2024-07-24 03:04:31 +0100

    init

--- src/main.rs
+++ src/main.rs
@@ -6,13 +6,15 @@ mod vga_buffer;
 
 
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
 }
 
 #[no_mangle]
 //remove the c zero runtime to run on baremetal target
 pub extern "C" fn _start() -> ! {
-    vga_buffer::print_something();
+    println!("Hello {}", 10);
+    panic!("I JUST PEEPEEPOEED MYSELF");
     loop {}
 }
